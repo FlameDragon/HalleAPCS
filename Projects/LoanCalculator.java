9a191020cce7e8b8c872f0f4a203c515270b7856
Halle Wong <Wong.Hall2020@ssusd.org>
2018-02-20 17:24:58 -0800

    adding stuff

---
+++ Projects/LoanCalculator.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class LoanCalculator{
+    public static int mode = -1;
+    public static double loanAmt = -1;
+    public static int loanTerm = -1;
+    public static double interestRate = -1;
+
+    public static void main(String[] args) {
+        //Read in the loan calculation mode
+        Scanner scan;
+        while(true) { //Loop the mode selection menu until a valid input is provided
+            scan = new Scanner(System.in);
+            printModeStatement(); //Call a function to print our mode selection statement
+            if(scan.hasNextInt()) { //Check if input provided is a valid integer
+                mode = scan.nextInt();
+                if(0 < mode && mode < 4) { //Check if input provided is 1, 2, or 3
+                    break; //Valid input has been provided. Break out of the while loop
+                }
+            }
+            //If it reaches here, the input was invalid. Loop again.
+        }
+        System.out.println("You selected mode " + mode);
+
+        if(mode == 1) {
+            flatInterest();
+        }
+        else if (mode == 2) { compoundinginterestnopayment ();
+                       
+        }
+        else if (mode == 3) { compoundinginterestwithpayment ();
+        }
+    }
+
+    public static void flatInterest() { Scanner scan;
+        while(true) { //Loop the mode selection menu until a valid input is provided
+            scan = new Scanner(System.in);
+            printModeStatement(); //Call a function to print our mode selection statement
+            if(scan.hasNextInt()) { //Check if input provided is a valid integer
+                mode = scan.nextInt();
+                if(0 < mode && mode < 4) { //Check if input provided is 1, 2, or 3
+                    break; //Valid input has been provided. Break out of the while loop
+                }
+            }
+            //If it reaches here, the input was invalid. Loop again.
+        }
+        System.out.println("You selected mode " + mode);
+
+        
+    }
+
+public static void compoundinginterestnopayment() {
+        System.out.println("Still a stub function. Not yet implemented"); //Delete this once you implement the function
+        //Calculate flat interest and print it out here
+    }
+
+public static void compoundinginterestwithpayment() {
+        System.out.println("Still a stub function. Not yet implemented"); //Delete this once you implement the function
+        //Calculate flat interest and print it out here
+    }
+    public static void printModeStatement() {
+        System.out.print(
+            "Select an interest calculation mode:\n"
+            + "(1) Flat Interest\n"
+            + "(2) Compounding Interest Without Monthly Payments\n"
+            + "(3) Compounding Interest With Monthly Payments\n"
+        );
+
+
+
+        
+    }
+}
