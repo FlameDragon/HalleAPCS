c03580f24e9f867881675325611cc83d3a11ded4
Halle Wong <Wong.Hall2020@ssusd.org>
2018-04-05 17:05:02 -0700

    plinko done

--- Plinko.java
+++ Plinko.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
-class Plinko {
+public class Plinko {
     public static final int SINGLE_DISC = 1;
     public static final int MULTI_DISC = 2;
     public static final int TERMINATE = 3;
     public static final int[] VALUES = {1, 3, 2, 0, 5, 0, 2, 3, 1};
     public static int mode = -1;
     public static int position = 0;
+    public static int NumDiscs = 0;
+
     public static void main(String[] args) {
         int slot = -1;
         Scanner scan;
@@ -23,52 +25,103 @@ class Plinko {
                         System.out.println("Enter a slot for the disc ");
                         if(scan.hasNextInt()) { 
                             position = scan.nextInt();
-                            if(position > 0 && position < 9) { 
+                            if(position > -1 && position < 9) { 
                                 slot = position * 2;
                                 break; //Valid input has been provided. Break out of the while loop
                             }
                         }
-                    }
-                     printEvenRow(slot);
-                    for (int x = 0; x <=5; x++) {
-                    for (int i = 0; i <=0; i++) 
-                        { if(Math.random() > .5) {
-                             slot ++;
-                             } else {
-                             slot--;
-                             }
+                    } 
+                
+                    printEvenRow(slot);
+                    for(int i = 0 ; i<=5; i++) {
+                        if(slot==0){slot++;}
+                        else if(slot==16){slot--;}
+                        else if(Math.random() > .5) { slot++; } 
+                        else { slot--; }
+                        
                         printOddRow(slot); 
                         
-                    }
-                    for (int i = 0; i <=0; i++) 
-                        { if(Math.random() > .5) {
-                             slot ++;
-                             } else {
-                             slot--;
-                             }
-                        for (int y = 0; y <=0; y++) 
-                        { if(Math.random() > .5) {
-                             slot ++;
-                             } else {
-                             slot--;
-                             }
-                        printEvenRow(slot); 
+                        if(slot==0){slot++;}
+                        else if(slot==16){slot--;}
+                        else if(Math.random() > .5) { slot++; } 
+                        else { slot--; }
                         
-                    } 
-                        }   
+                        printEvenRow(slot);
+                        
                     }
                 }
-                else if(mode == MULTI_DISC) {
-                    System.out.println("Mode not yet implemented");
+                else if(mode == MULTI_DISC)  { 
+                    while(true) { //Loop the mode selection menu until a valid input is provided
+                        scan = new Scanner(System.in);
+                        System.out.println("Enter a slot for the disc ");
+                        if(scan.hasNextInt()) { 
+                            position = scan.nextInt();
+                            if(position > -1 && position < 9) { 
+                                slot = position * 2;
+                                break; //Valid input has been provided. Break out of the while loop
+                            }
+                        }
+                    } 
+                    while(true) { //Loop the mode selection menu until a valid input is provided
+                        scan = new Scanner(System.in);
+                        System.out.println("Enter an amount of discs ");
+                        if(scan.hasNextInt()) { 
+                            NumDiscs = scan.nextInt();
+                            if(NumDiscs > -1) { 
+                                break; //Valid input has been provided. Break out of the while loop
+                            }
+                        }
+                    } 
+                    
+                    int[] slotArray = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+                    for(int x= 0 ; x < NumDiscs; x++){
+                       // System.out.println(position);
+                        slot = position * 2;
+                        for(int i = 0 ; i<=5; i++) { 
+                            if(slot==0){slot++;}
+                            else if(slot==16){slot--;}
+                            else if(Math.random() > .5) { slot++; } 
+                            else { slot--; }
+                    
+                            if(slot==0){slot++;}
+                            else if(slot==16){slot--;}
+                            else if(Math.random() > .5) { slot++; } 
+                            else { slot--; }
+                            
+                        }
+                        slotArray[slot/2]++;
+                    } 
+
+                    System.out.println("Number of discs at Position 0:"+ " " + slotArray[0] ); 
+                    System.out.println("Number of discs at position 1:"+ " " + slotArray[1] ); 
+                    System.out.println("Number of discs at Position 2:"+ " "  + slotArray[2] ); 
+                    System.out.println("Number of discs at Position 3:"+ " " + slotArray[3] ); 
+                    System.out.println("Number of discs at Position 4:"+ " " + slotArray[4] ); 
+                    System.out.println("Number of discs at Position 5:"+  " " + slotArray[5] ); 
+                    System.out.println("Number of discs at Position 6:"+  " " + slotArray[6] ); 
+                    System.out.println("Number of discs at Position 7:"+  " " + slotArray[7] ); 
+                    System.out.println("Number of discs at Position 8:"+  " " + slotArray[8] ); 
+                    int totalPoints = 
+                    slotArray[0]*VALUES[0]+
+                    slotArray[1]*VALUES[1]+
+                    slotArray[2]*VALUES[2]+
+                    slotArray[3]*VALUES[3]+
+                    slotArray[4]*VALUES[4]+
+                    slotArray[5]*VALUES[5]+
+                    slotArray[6]*VALUES[6]+
+                    slotArray[7]*VALUES[7]+
+                    slotArray[8]*VALUES[8];
+                    System.out.println("Total Points"+ " " + totalPoints);
                 }
-                else if(mode == TERMINATE) {
+                if(mode == TERMINATE) {
                     System.out.println("Goodbye");
                     break;
                 } else {
                     continue;
                 }
-            }
-        }
+            } 
+        } 
     }
 
     public static void printOddRow(int slot) {
