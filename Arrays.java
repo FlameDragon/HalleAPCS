121720c9b5e8d25ec3e0e89a1a24f8f04309bad8
Halle Wong <Wong.Hall2020@ssusd.org>
2018-04-10 16:27:56 -0700

    adding Arrays

---
+++ Arrays.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Arrays {
+   
+ 
+    public static void main(String[] args) {
+       int [] numArray = {20, 3, 6, 10, 13, 25, 100, 6, 3, 20, 5102};
+
+    System.out.println( numArray[3] ); //10
+
+    System.out.println( numArray[10] ); //5102
+
+    System.out.println( numArray[1] ); //3
+
+    System.out.println( numArray[7] ); //6
+
+    System.out.println( numArray[8] ); //3
+    
+    System.out.println( numArray[4] );
+
+    System.out.println( numArray[1] );
+
+    System.out.println( numArray[10] );
+
+    System.out.println( numArray[5] );
+
+    System.out.println( numArray[0] );
+
+int[] fibonacciArray = {1, 1, 2, 3, 0, 8, 13, 0, 34, 0};
+fibonacciArray[4] = fibonacciArray[2] + fibonacciArray[3] ;
+   System.out.println( fibonacciArray[4] );
+fibonacciArray[7] = fibonacciArray[5]+fibonacciArray[6] ;
+    System.out.println( fibonacciArray[7] );
+fibonacciArray[9] = fibonacciArray[7]+fibonacciArray[8] ;
+    System.out.println( fibonacciArray[9] );
+
+   
+
+
+    }
+    }
+
