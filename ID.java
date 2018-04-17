07d570b598ee804ce842f156fb4f37efa9735d9d
Halle Wong <Wong.Hall2020@ssusd.org>
2018-04-17 16:44:01 -0700

    iterating

--- ID.java
+++ ID.java
@@ -6,16 +6,58 @@ public class ID {
     public static void main(String[] args) {
        
        
-    int[] nums = new int[201];
    
-  
-    int[] powers = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+   
+        int[] powers = new int[201];
 
-    for(int value : powers) {
-        System.out.println(value);
-    }    
+        powers[0] = -100;
+        //#1
+        for(int i = 1; i < powers.length; i++) {
+            powers[i] = powers[i-1] +1 ;
+        }
+        
 
+        //#2
+        for(int i = 0; i < powers.length; i++) {
+            
+            System.out.println(Math.abs (powers[i]));
+        }
+
+        //#3
+        int[] fib = new int [40];
+        fib[0]=0;
+        fib[1]=1;
+        System.out.println(fib[0]);
+        System.out.println(fib[1]);
+        for(int i =2; i <fib.length;i++) 
+        { 
+            fib[i] = fib[i-1] + fib[i-2] ;
+            System.out.println (fib[i]);
+
+        }
+
+        //#4
+
+        int[] numbers = new int[(int) (Math.random() * 500)+1];
+        for(int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (Math.random() * 500);
+        }
+        for(int number : numbers) {
+            if(number % 2 == 1) {
+                System.out.println(number); 
+            }
+        }
+
+        String[] messages = new String [&];
+
+        for(String message : messages) {
+             if(message.length() >10) {
+                System.out.println(message); 
+             }
+        }
 
     }
-    }
+
+}
+
 
