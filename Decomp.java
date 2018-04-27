17f881ef1067ceb786beab043f49b177369699e6
Halle Wong <Wong.Hall2020@ssusd.org>
2018-04-26 17:17:55 -0700

    arrays

---
+++ Decomp.java
@@ -0,0 +1,56 @@
+
+public class Decomp {
+
+
+ public static void main(String[] args) {
+   
+   int[][] twoDArray = new int[5][5];
+
+    for(int i = 0; i < twoDArray.length; i++) { System.out.println("");
+        for(int j = 0; j < twoDArray[i].length; j++) {
+            twoDArray[i][j] = (i *-5) + (-j -1);
+            System.out.print(twoDArray[i][j] + " ");
+       
+       
+        }
+    }
+
+int[][] twoDArry = new int[3][3];
+int[][] twoArry = new int[twoDArray.length][twoDArray[0].length];
+    for(int i = 0; i < twoDArry.length; i++) { System.out.println("");
+        for(int j = 0; j < twoDArry[i].length; j++) {
+            twoDArry[i][j] = (3*i * 3) + (3*j + 3 );
+            System.out.print(twoArry[i][j] + " ");
+       
+       
+        }
+    }
+
+// int[] numArray = {10, 20, 30, 40, 50}; 
+// int[] results = new int [numArray.length];
+//     results[0] = numArray[numArray.length-1];
+//     results[1] = numArray[numArray.length-2];
+//     results[2] = numArray[numArray.length-3];
+//     results[3] = numArray[numArray.length-4];
+//     results[4] = numArray[numArray.length-5];
+    
+//     System.out.println( numArray[0] ); 
+//     System.out.println( numArray[1] ); 
+//     System.out.println( numArray[2] ); 
+//     System.out.println( numArray[3] ); 
+//     System.out.println( numArray[4] );
+    
+    
+    
+    
+    
+    
+        }
+    }
+ 
+
+
+    
+   
+
+
